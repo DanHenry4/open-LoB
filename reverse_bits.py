009b9c17bb062b0b10b66e6336ba0dafff641bc7
Daniel Henry <dhenry4@gmail.com>
2019-03-25 17:53:22 -0400

    Added some comments to clarify certain parts.

--- reverse_bits.py
+++ reverse_bits.py
@@ -1,8 +1,19 @@
-ALPHABASE = "abcdefghijklmnopqrstuvwxyz,. "
-LENGTH = 3200
-BITS = 15546
+# TODO figure out how to guarantee converting base 10 to ALPHABASE
+# yields an output of length LENGTH. Sometimes the output returns
+# a subrandom sequence larger or smaller than LENGTH by 1.
 
 DEBUG = True
+
+# Alpha-numeric-symbolic characters the pages are allowed to use.
+ALPHABASE = "abcdefghijklmnopqrstuvwxyz,. "
+
+# Length of each page in characters.
+LENGTH = 3200
+
+# We want len(ALPHABASE)**LENGTH to be about the same as the 2**x.
+# e.g. 29**3200 ≈ 2**15546
+# So far I've used WolframAlpha to find the equivalencies, but there might be a way to do it in Python.
+BITS = 15546
 
 def main():
 	ed = input('[e]ncode or [d]ecode? ')
@@ -16,9 +27,10 @@ def main():
 		print(x_rev)
 
 def page_by_seed(seed):
-	# Reverse the bits to generate a subrandom number.
-	# See: https://stackoverflow.com/a/41728178/2368124
-	# for information on the Hammersley Set.
+	"""
+	Takes a base 10 seed, reverses the number bitwise, 
+	converts to base 29, in this case, and returns the result.
+	"""
 	seed_rev = reverse_bits(seed)
 	encoded_string = encode(seed_rev)
 	if DEBUG:
@@ -31,17 +43,23 @@ def seed_by_page(page):
 	return seed
 
 def reverse_bits(number):
+	"""
+	This is the Hammersley Set operation that reverses a number bitwise.
+	e.g. 1100011101 -> 1011100011
+	See: https://stackoverflow.com/a/41728178/2368124
+	for information on the Hammersley Set.
+	"""
 	b = '{:0{width}b}'.format(number, width=BITS)
 	return int(b[::-1], 2)
 
-"""
-Encode a positive number in Base X
-
-Arguments:
-- `num`: The number to encode
-- `alphabet`: The alphabet to use for encoding.
-"""
 def encode(num, alphabet=ALPHABASE):
+	"""
+	Encode a positive number in Base X
+
+	Arguments:
+	- `num`: The number to encode
+	- `alphabet`: The alphabet to use for encoding.
+	"""
 	if num == 0:
 		return alphabet[0]
 	arr = []
@@ -52,14 +70,14 @@ def encode(num, alphabet=ALPHABASE):
 	arr.reverse()
 	return ''.join(arr)
 
-"""
-Decode a Base X encoded string into the number.
-
-Arguments:
-- `string`: The encoded string
-- `alphabet`: The alphabet to use for encoding
-"""
 def decode(string, alphabet=ALPHABASE):
+	"""
+	Decode a Base X encoded string into the number.
+
+	Arguments:
+	- `string`: The encoded string
+	- `alphabet`: The alphabet to use for encoding
+	"""
 	base = len(alphabet)
 	strlen = len(string)
 	num = 0
